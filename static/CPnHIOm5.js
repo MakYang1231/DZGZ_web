ea8a19de251b8936ac44124009037df26d590240
Mak <makyang.test@gmai.com>
2024-05-02 04:26:31 +0800

    Updates

--- static/CPnHIOm5.js
+++
@@ -1,3 +0,0 @@
-import{d as e,u as t,a,o as n}from"./rlhfd34B.js";const c=e({__name:"prize",setup(o){return t().currentRoute.value,(r,s)=>(n(),a("div",null,`
-        static page
-    `))}});export{c as default};
---
+++ static/hHzNjAfb.js
@@ -0,0 +1,3 @@
+import{d as e,u as t,o as a,a as n}from"./DEemp6dc.js";const c=e({__name:"prize",setup(o){return t().currentRoute.value,(r,s)=>(a(),n("div",null,`
+        static page
+    `))}});export{c as default};
