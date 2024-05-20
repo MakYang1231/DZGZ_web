824e7e01c22b4f5a0f2a87645ac3a356abaacd6b
Mak <makyang.test@gmai.com>
2024-05-20 17:13:14 +0800

    Updates

--- static/ujHXNrdJ.js
+++
@@ -1,3 +0,0 @@
-import{d as e,u as t,a,b as n}from"./uzP5vnAt.js";const c=e({__name:"prize",setup(r){return t().currentRoute.value,(o,s)=>(a(),n("div",null,`
-        static page
-    `))}});export{c as default};
