7eac04ca73b34b246c823b2842e47e5d5210df53
Mak <makyang.test@gmai.com>
2024-05-20 17:22:15 +0800

    Updates

---
+++ static/BYkwGO6u.js
@@ -0,0 +1,3 @@
+import{d as e,u as t,a,b as n}from"./DG1tayHD.js";const c=e({__name:"prize",setup(r){return t().currentRoute.value,(o,s)=>(a(),n("div",null,`
+        static page
+    `))}});export{c as default};
