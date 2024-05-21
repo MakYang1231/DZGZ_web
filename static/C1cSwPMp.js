cfccad54aef1d6b2bba1ec9e4ea5b94332898898
Mak <makyang.test@gmai.com>
2024-05-21 21:54:52 +0800

    Updates

---
+++ static/C1cSwPMp.js
@@ -0,0 +1,3 @@
+import{u as e}from"./DbszNA4r.js";import{d as t,z as r,A as n}from"./orbDqLiy.js";const p=t({__name:"prize",setup(o){return e().currentRoute.value,(a,s)=>(r(),n("div",null,`
+        static page
+    `))}});export{p as default};
