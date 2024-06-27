37dfc080e65ec4dc668e204d8065c03d1abac80f
Mak <makyang.test@gmai.com>
2024-06-27 20:40:29 +0800

    Updates

--- static/DyxZtfx5.js
+++
@@ -1,1 +0,0 @@
-import{d as o,z as s,A as a,Q as n}from"./CB_E7rYp.js";const c=o({__name:"BootstrapIcon",props:{name:{}},setup(r){return(e,p)=>(s(),a("i",{class:n("bi bi-"+e.$props.name)},null,2))}});export{c as _};
