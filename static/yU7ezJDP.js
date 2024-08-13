5e38072287559e7a6d27bcc6d988863be840db8c
Mak <makyang.test@gmai.com>
2024-08-13 20:29:15 +0800

    Updates

---
+++ static/yU7ezJDP.js
@@ -0,0 +1,1 @@
+import{d as o,y as s,z as a,P as n}from"./CTCtpd9K.js";const c=o({__name:"BootstrapIcon",props:{name:{}},setup(r){return(e,p)=>(s(),a("i",{class:n("bi bi-"+e.$props.name)},null,2))}});export{c as _};
